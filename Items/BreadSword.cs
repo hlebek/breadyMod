42a9172936e29f62d4fd92a1b938f6b7ab88a202
hlebek <frendzel0000@gmail.com>
2021-11-10 13:57:49 +0100

    Test

---
+++ Items/BreadSword.cs
@@ -0,0 +1,40 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace breadyMod.Items
+{
+	public class BreadSword : ModItem
+	{
+		public override void SetStaticDefaults() 
+		{
+			// DisplayName.SetDefault("BreadSword"); // By default, capitalization in classnames will add spaces to the display name. You can customize the display name here by uncommenting this line.
+			Tooltip.SetDefault("This is a sample sword to test things");
+		}
+
+		public override void SetDefaults() 
+		{
+			item.damage = 1;
+			item.melee = true;
+			item.width = 40;
+			item.height = 40;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = 1;
+			item.knockBack = 100;
+			item.value = 10000;
+			item.rare = 2;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+            item.shoot = ProjectileID.WoodenArrowFriendly;
+		}
+
+		public override void AddRecipes() 
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemID.DirtBlock, 1);
+			recipe.AddTile(TileID.WorkBenches);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+	}
+}
---
+++ breadyMod.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace breadyMod
+{
+	public class breadyMod : Mod
+	{
+	}
+}
